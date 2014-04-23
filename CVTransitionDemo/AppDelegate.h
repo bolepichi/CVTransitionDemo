3048b71eb80f6583ad82d9f800a24d7d9c532d1b
Taagoo'iMac <apple@AppledeiMac.local>
2014-04-23 11:21:30 +0800

    Initial Commit

---
+++ CVTransitionDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CVTransitionDemo
+//
+//  Created by Taagoo'iMac on 14-4-23.
+//  Copyright (c) 2014年 Taagoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
